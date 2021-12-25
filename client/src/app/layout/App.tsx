7a1fb9c53a76b1fb38d65392ecfda704d369a412
EleanorKal <help.sleekmya@gmail.com>
2021-12-25 15:00:37 +0100

    end of section three

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -18,45 +18,39 @@
 // the elipses is known as a 'spread operator' which indicates that a range will be declared
 
 
-import { Card, Container, CssBaseline, Grid } from "@mui/material";
-import { useEffect, useState } from "react";
+
+import { Container, CssBaseline } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
-import { Product } from "../models/product";
 import Header from "./Header";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
+  const[darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light'
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  })
 
-
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
-
-
-  function addProduct() {
-    setProducts(prevState => [...prevState,
-    {
-      id: prevState.length + 101,
-      name: 'product' + (prevState.length + 1),
-      price: (prevState.length * 100) + 100,
-      brand: 'some brand',
-      description: 'some description',
-      pictureUrl: 'http://picsum.photos/200'
-    }])
-
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
   }
 
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog products={products} addProduct={addProduct} />
+        <Catalog/>
       </Container>
 
-    </>
+    </ThemeProvider>
   );
 }
 
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,23 +1,25 @@
 // in React the interface method adds functions to the props
 // the interface method specifies that the properties from the Product app component
 // are required to be passed to the interface component
-import { Button } from "@mui/material";
+import { useState, useEffect } from "react";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 
 
-interface Props {
-    products: Product[];
-    addProduct: () => void;
-}
+export default function Catalog() {
+    const [products, setProducts] = useState<Product[]>([]);
 
 
-export default function Catalog({products, addProduct}: Props) 
-{
+    useEffect(() => {
+      fetch('http://localhost:5000/api/products')
+        .then(response => response.json())
+        .then(data => setProducts(data))
+    }, [])
+  
+
     return (
         <>
             <ProductList products={products} />
-            <Button variant='contained' onClick={addProduct}>Add product</Button>
         </>
     )
 }
